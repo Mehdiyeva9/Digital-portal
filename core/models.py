2468f75362faebc6a6d9c315dd2a80c08118e4e5
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-08-29 11:06:59 +0300

    Update 2

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Collab(models.Model):
+    name = models.CharField(max_length=50)
+    icon = models.ImageField(upload_to="digit_imgs")
+
+    def __str__(self):
+        return self.name
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=256)
+    company = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=12)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+    
+class Programs(models.Model):
+    name = models.CharField(max_length=200)
+    duration = models.CharField(max_length=25)
+    image = models.ImageField(upload_to="digit_imgs")
+    price = models.IntegerField(default=0)
+    about = models.TextField()
+
+    
