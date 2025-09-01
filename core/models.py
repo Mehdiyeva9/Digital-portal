87661cfd367cf2b35cfcc92ad4efdba039ccb2e6
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-09-01 15:54:59 +0300

    Update 4

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
 class Collab(models.Model):
     name = models.CharField(max_length=50)
@@ -40,3 +41,16 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.content[:50]
+    
+class SiteSettings(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=150)
+    
