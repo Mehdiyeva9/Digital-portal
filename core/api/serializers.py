5aa66f73d80fa659676e74b96081edb12444d55e
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-09-08 15:40:09 +0300

    Update 8

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -15,3 +15,23 @@ class ContactFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactForm
         fields = "__all__"
+
+class ProgramSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Program
+        fields = "__all__"
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+class SiteSettingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SiteSettings
+        fields = "__all__"
