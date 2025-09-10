291488a59b8907306350ad7291731bef0ea49f7e
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-09-10 21:22:55 +0300

    Update 10

--- core/api/views.py
+++ core/api/views.py
@@ -6,3 +6,16 @@ from django.contrib.auth.models import User
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+
+class CollabListAPIView(ListAPIView):
+    queryset = Collab.objects.all()
+    serializer_class = CollabSerializer
+
+class OurServiceListAPIView(ListAPIView):
+    queryset = OurService.objects.all()
+    serializer_class = OurServiceSerializer
+
+class ContactFormListAPIView(ListAPIView):
+    queryset = ContactForm.objects.all()
+    serializer_class = ContactFormSerializer
+
